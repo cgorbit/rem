4f53060208f27c560ce847bafba042c2e4bb5f2d
Trofimenkov Kirill <cgorbit@ya.ru>
2015-10-09 12:19:49 +0300

    Output packets' extended info in reset messages

--- rem/messages.py
+++ rem/messages.py
@@ -2,8 +2,29 @@ import cStringIO
 import time
 from packet import PacketState, PacketFlag
 
+def utf8ifunicode(str):
+    return str.encode('utf-8') if isinstance(str, unicode) else str
 
-class IMessageHelper(object): pass
+class IMessageHelper(object):
+    @classmethod
+    def _outputJobResults(cls, out, job):
+        if job.get("results"):
+            print >>out, "\n".join("\tresult: %s" % v for v in job.get("results"))
+
+    @classmethod
+    def _outputExtendedPacketStatus(cls, out, pck_id, pck):
+        print >>out, "Extended packet status:"
+        print >>out, "packet id:", pck_id
+        print >>out, "\n".join("%s: %s" % (k, v) for k, v in pck.iteritems() if k not in ("jobs", "history"))
+        print >>out, "history:"
+        for state, timestamp in pck.get("history", []):
+            print >>out, "\t%s: %s" % (time.ctime(timestamp), state)
+        for job in pck.get("jobs", []):
+            print >>out, "--== subjob info ==--"
+            for k in sorted(job):
+                if k != "results":
+                    print >>out, "\t%s: %s" % (k, utf8ifunicode(job.get(k, "N/A")))
+            cls._outputJobResults(out, job)
 
 
 def GetHelper(helper_cls, *args):
@@ -34,7 +55,6 @@ def GetResetNotificationHelper(pck, ctx, message):
     if ctx:
         return ResetNotification(pck, ctx, message)
 
-
 class PacketExecutionError(IMessageHelper):
     def __init__(self, pck, ctx):
         self.pck = pck
@@ -49,34 +69,16 @@ class PacketExecutionError(IMessageHelper):
     def message(self):
         mbuf = cStringIO.StringIO()
 
-        def appendJobItem(job, itemName):
-            print >> mbuf, "\t%s: %s" % (itemName, job.get(itemName, "N/A"))
-
-        def appendJobResults(job):
-            if job.get("results"):
-                print >> mbuf, "\n".join("\tresult: %s" % v for v in job.get("results"))
-
         print >> mbuf, "Packet '%(pname)s' has been aborted because of some error states" % {"pname": self.pck.name}
         p_state = self.pck.Status()
         jobs = p_state.get("jobs", [])
         for job in jobs:
             if job.get("state") == "errored":
                 print >> mbuf, "--== failed subjob ==--"
-                appendJobItem(job, "shell")
-                appendJobResults(job)
+                print >> mbuf, "\t%s: %s" % ('shell', utf8ifunicode(job.get('shell', "N/A")))
+                self._outputJobResults(mbuf, job)
         print >> mbuf, "=" * 80
-        print >> mbuf, "Extended packet status:"
-        print >> mbuf, "packet id:", self.pck.id
-        print >> mbuf, "\n".join("%s: %s" % (k, v) for k, v in p_state.iteritems() if k not in ("jobs", "history"))
-        print >> mbuf, "history:"
-        for state, timestamp in p_state.get("history", []):
-            print >> mbuf, "\t%s: %s" % (time.ctime(timestamp), state)
-        for job in jobs:
-            print >> mbuf, "--== subjob info ==--"
-            for k in sorted(job):
-                if k != "results":
-                    appendJobItem(job, k)
-            appendJobResults(job)
+        self._outputExtendedPacketStatus(mbuf, self.pck.id, p_state)
         return mbuf.getvalue()
 
 
@@ -176,8 +178,9 @@ class ResetNotification(IMessageHelper):
         print >>mbuf, "may be your packet '%(pname)s' had to reset, but haven't" % {"pname": self.pck.name}
         if not self.pck.isResetable:
             print >>mbuf, "... because your packet doesn't allow automatical resets"
-        print >>mbuf, "packet id:", self.pck.id
         print >>mbuf, "reset reason:", self.reason
+        print >>mbuf
+        self._outputExtendedPacketStatus(mbuf, self.pck.id, self.pck.Status())
         return mbuf.getvalue()
 
 
