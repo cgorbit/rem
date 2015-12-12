ffeaf958d7c614a7b5ee8e2bcfa2cda7af4be489
Trofimenkov Kirill <cgorbit@ya.ru>
2015-12-12 20:46:02 +0300

    better overengineering

--- rem/runner.py
+++ rem/runner.py
@@ -11,6 +11,8 @@ from collections import deque
 from tempfile import NamedTemporaryFile
 from subprocess import CalledProcessError, MAXFD
 import atexit
+import types
+import errno
 
 from future import Promise
 from profile import ProfiledThread
@@ -34,7 +36,7 @@ class StopServiceResponseMessage(object):
     pass
 
 class NewTaskParamsMessage(object):
-    def __init__(self, args, stdin=None, stdin_content=None, stdout=None, stderr=None, setpgrp=False, cwd=None):
+    def __init__(self, args, stdin=None, stdin_content=None, stdout=None, stderr=None, setpgrp=False, cwd=None, shell=False):
         self.task_id = None
         self.args = args
         self.stdin = stdin # string or None
@@ -43,6 +45,7 @@ class NewTaskParamsMessage(object):
         self.stderr = stderr # filename or None
         self.setpgrp = setpgrp
         self.cwd = cwd # filename or None
+        self.shell = shell # filename or None
 
 def set_cloexec(fd):
     fcntl.fcntl(fd, fcntl.F_SETFD, fcntl.FD_CLOEXEC | fcntl.fcntl(fd, fcntl.F_GETFD))
@@ -108,7 +111,7 @@ def deserialize(stream):
         #self.msg_len_str = serialize_to_string(len(self.msg))
         #self.len_
 
-class _Executor(object):
+class _Server(object):
     def __init__(self, channel):
         signal.signal(signal.SIGCHLD, self._sig_chld_handler)
         for sig in [signal.SIGINT, signal.SIGTERM]:
@@ -123,8 +126,8 @@ class _Executor(object):
         set_cloexec(self._channel.fileno())
         self._should_stop = False
         self._lock = threading.Lock()
-        self._running = {}
-        self._running_not_empty = threading.Condition(self._lock)
+        self._active = {}
+        self._running_count = 0
         self._send_queue = deque()
         self._send_queue_not_empty = threading.Condition(self._lock)
         self._read_thread = ProfiledThread(target=self._read_loop, name_prefix='RunnerSrvRd')
@@ -148,7 +151,7 @@ class _Executor(object):
 
     def wait(self):
 # TODO
-        #logging.debug('_Executor.wait started')
+        #logging.debug('_Server.wait started')
 # Fuck Python
         while not(self._read_stopped and self._write_stopped):
             self._read_thread.join(1) # sleeps for 50ms actually
@@ -156,49 +159,54 @@ class _Executor(object):
         self._read_thread.join()
         self._write_thread.join()
 
+    class RunningTask(object):
+        def __init__(self, task_id):
+            self.task_id = task_id
+            self.start_sent = False
+            self.exit_status = None
+            self.exec_errored = False
+
 # TODO TODO TODO REFACTOR
     def _start_process(self, task):
         exec_err_rd, exec_err_wr = os.pipe()
 
-        logging.debug('RUN %r' % task.__dict__)
-        os.write(2, 'RUN %r\n' % task.__dict__)
+        #logging.debug('RUN %r' % task.__dict__)
+        #os.write(2, 'RUN %r\n' % task.__dict__)
 
+        running_task = None
         try:
-            #logging.debug('before_fork')
-            pid = os.fork()
-            #if pid:
-                #logging.debug('after_fork')
+            t0 = time.time()
+            with self._lock:
+                pid = os.fork()
+                if pid:
+                    running_task = self._active[pid] = self.RunningTask(task.task_id)
+                    self._running_count += 1
+            _fork_time = time.time() - t0
         except OSError as e:
             return None, str(e)
 
-        if pid: # TODO
-            self._running[pid] = task.task_id # TODO
+        if pid:
+            logging.debug('fork time %s' % _fork_time)
 
         if pid:
             os.close(exec_err_wr)
             exec_error = None
             try:
-                #in_ = os.fdopen(exec_err_rd)
-                #exec_error = pickle.loads(in_.read())
-                ##in_.close()
-                #del in_
-
                 with os.fdopen(exec_err_rd) as in_:
                     s = in_.read()
                     if s:
                         exec_error = pickle.loads(s)
-
-                #str = os.read(exec_err_rd, 4096)
-                #exec_error = pickle.loads(str) if str else None
             except BaseException as exec_error:
                 try:
                     os.close(exec_err_rd)
                 except:
                     pass
+
+            running_task.exec_errored = bool(exec_error)
             return pid, exec_error or None
 
         else:
-            code = 0
+            exit_code = 0
             try:
                 set_cloexec(exec_err_wr)
                 os.close(exec_err_rd)
@@ -206,12 +214,12 @@ class _Executor(object):
                 if task.stdin is not None:
                     dup2file(task.stdin, 0, os.O_RDONLY)
                 elif task.stdin_content is not None:
-                    with NamedTemporaryFile() as tmp: # TODO sorry
+                    with NamedTemporaryFile() as tmp: # TODO Remove implementation
                         tmp.write(task.stdin_content)
                         tmp.flush()
                         dup2file(tmp.name, 0, os.O_RDONLY)
-                else:
-                    dup2devnull(0, os.O_RDONLY)
+                #else:
+                    #dup2devnull(0, os.O_RDONLY)
 
                 write_flags = os.O_WRONLY | os.O_CREAT | os.O_TRUNC
 
@@ -222,8 +230,8 @@ class _Executor(object):
 
                 if task.stderr is not None:
                     dup2file(task.stderr, 2, write_flags)
-                else:
-                    dup2devnull(2, os.O_WRONLY)
+                #else:
+                    #dup2devnull(2, os.O_WRONLY)
 
                 if task.setpgrp:
                     os.setpgrp()
@@ -232,7 +240,22 @@ class _Executor(object):
                     os.chdir(task.cwd)
 
                 #os.write(2, '++ RUN %r\n' % task.args)
-                os.execvp(task.args[0], task.args)
+                args = task.args
+
+                if isinstance(args, types.StringTypes):
+                    args = [args]
+                else:
+                    args = list(args)
+
+                if task.shell:
+                    args = ["/bin/sh", "-c"] + args
+                    #if executable:
+                        #args[0] = executable
+
+                #if executable is None:
+                    #executable = args[0]
+
+                os.execvp(args[0], args)
 
             except BaseException as e:
                 try:
@@ -241,36 +264,38 @@ class _Executor(object):
                         out.flush()
                 except:
                     pass
-                code = 1
-            os._exit(code)
+                exit_code = 1
+            os._exit(exit_code)
 
 
     def _read_loop(self):
         channel_in = self._channel_in
         stop_request_received = False
 
-        logging.debug('_Executor._read_loop started')
+        logging.debug('_Server._read_loop started')
 
         while True:
             try:
                 msg = deserialize(channel_in)
             except EOFError:
-                logging.debug('_Executor._read_loop EOFError')
+                logging.debug('_Server._read_loop EOFError')
                 break
 
             if isinstance(msg, NewTaskParamsMessage):
-                #logging.debug('_Executor._read_loop NewTaskParamsMessage received')
+                #logging.debug('_Server._read_loop NewTaskParamsMessage received')
 
                 if stop_request_received:
                     raise RuntimeError("Message in channel after StopServiceRequestMessage")
 
+                t0 = time.time()
+                pid, error = self._start_process(msg)
                 with self._lock:
-                    pid, error = self._start_process(msg)
-                    self._send_queue.append(ProcessStartMessage(msg.task_id, pid, error))
+                    self._enqueue_start_msg(msg.task_id, pid, error)
                     self._send_queue_not_empty.notify()
+                logging.debug('_start_process time %s' % (time.time() - t0))
 
             elif isinstance(msg, StopServiceRequestMessage):
-                #logging.debug('_Executor._read_loop StopServiceRequestMessage received')
+                #logging.debug('_Server._read_loop StopServiceRequestMessage received')
                 stop_request_received = True
                 with self._lock:
                     self._should_stop = True
@@ -290,22 +315,26 @@ class _Executor(object):
         channel_out = self._channel_out
         send_queue = self._send_queue
 
-        logging.debug('_Executor._write_loop started')
+        logging.debug('_Server._write_loop started')
 
         while True:
             last_iter = False
 
             with self._lock:
-                while not send_queue and not (self._should_stop and not self._running):
+                while not send_queue and not (self._should_stop and not self._active):
+                    logging.info('before self._send_queue_not_empty.wait: %r' % dict(
+                        send_queue=len(send_queue), should_stop=self._should_stop,
+                        active=len(self._active), running=self._running_count,
+                    ))
                     self._send_queue_not_empty.wait()
 
                 messages = []
                 while send_queue:
-                    messages.append(send_queue.pop())
+                    messages.append(send_queue.popleft())
 
-                if self._should_stop and not self._running:
+                if self._should_stop and not self._active:
                     last_iter = True
-                    logging.debug('_Executor._write_loop append(StopServiceResponseMessage)')
+                    logging.debug('_Server._write_loop append(StopServiceResponseMessage)')
                     messages.append(StopServiceResponseMessage())
 
             for msg in messages:
@@ -320,18 +349,51 @@ class _Executor(object):
     # TODO
         self._write_stopped = True
 
+    def _enqueue_start_msg(self, task_id, pid, error):
+        task = None
+        if pid:
+            task = self._active[pid]
+
+        terminated = task and task.exit_status is not None
+
+        if terminated:
+            self._active.pop(pid)
+
+        if error:
+            pid = None
+
+        self._send_queue.append(
+            ProcessStartMessage(task_id, pid, error))
+
+        task.start_sent = True
+
+        if terminated and not task.exec_errored:
+            self._send_queue.append(
+                ProcessTerminationMessage(task_id, task.exit_status))
+
+    def _enqueue_term_msg(self, pid, exit_status):
+        task = self._active[pid]
+
+        if task.start_sent:
+            self._active.pop(pid)
+            if not task.exec_errored:
+                self._send_queue.append(
+                    ProcessTerminationMessage(task.task_id, exit_status))
+        else:
+            task.exit_status = exit_status
+
     def _sig_chld_handler(self, *args):
         with self._lock:
-            while self._running:
+            while self._running_count:
                 pid, status = noeintr(lambda : os.waitpid(-1, os.WNOHANG)) # noeintr not actually need
 
                 if pid == 0:
                     break
 
-                #logging.debug('_Executor._sig_chld_handler pid = %d, status = %d' % (pid, status))
+                #logging.debug('_Server._sig_chld_handler pid = %d, status = %d' % (pid, status))
 
-                task_id = self._running.pop(pid)
-                self._send_queue.append(ProcessTerminationMessage(task_id, status))
+                self._running_count -= 1
+                self._enqueue_term_msg(pid, status)
 
             self._send_queue_not_empty.notify()
 
@@ -382,10 +444,10 @@ class _Executor(object):
 
 
 class _Popen(object):
-    def __init__(self, pid, exit_status, send_signal):
+    def __init__(self, pid, exit_status): #, send_signal):
         self.pid = pid
         self._exit_status = exit_status
-        self._send_signal = send_signal
+        #self._send_signal = send_signal
         self.returncode = None
 
     def _handle_exitstatus(self, sts, _WIFSIGNALED=os.WIFSIGNALED,
@@ -412,6 +474,7 @@ class _Popen(object):
             return None
 
         self.returncode = self._handle_exitstatus(self._exit_status.get())
+        self._exit_status = None
 
         return self.returncode
 
@@ -436,7 +499,7 @@ class _Popen(object):
     def pipe_cloexec(self):
         raise NotImplementedError("pipe_cloexec not implemented")
 
-class _Interface(object):
+class _Client(object):
     class Task(object):
         def __init__(self):
             self.pid = Promise()
@@ -461,7 +524,7 @@ class _Interface(object):
         self._read_thread.start()
 
     def stop(self):
-        logging.debug('_Interface.stop()')
+        logging.debug('_Client.stop()')
         with self._lock:
             self._should_stop = True
             self._queue_not_empty.notify()
@@ -479,36 +542,38 @@ class _Interface(object):
         except:
             pass
 
-    def Popen(self, *args, **kwargs):
+    def start(self, *pargs, **pkwargs):
         with self._lock:
             task_id = self._next_task_id
             self._next_task_id += 1
 
-            msg = NewTaskParamsMessage(*args, **kwargs)
+            msg = NewTaskParamsMessage(*pargs, **pkwargs)
             msg.task_id = task_id
 
-            task = _Interface.Task()
+            task = _Client.Task()
             self._tasks[task_id] = task
 
             self._input_queue.append(msg)
             self._queue_not_empty.notify()
 
-        pid = task.pid.get_future().get()
+        def join_pid():
+            pid = task.pid.get_future().get()
 
-        def send_signal(sig):
-            return self._send_signal(task_id, sig)
+            return _Popen(
+                pid=pid,
+                exit_status=task.term_info.get_future(),
+            )
 
-        return _Popen(
-            pid=pid,
-            exit_status=task.term_info.get_future(),
-            send_signal=send_signal
-        )
+        return join_pid
+
+    def Popen(self, *pargs, **pkwargs):
+        return self.start(*pargs, **pkwargs)()
 
     def _write_loop(self):
         queue = self._input_queue
         channel_out = self._channel_out
 
-        logging.debug('_Interface._write_loop started')
+        logging.debug('_Client._write_loop started')
 
         while True:
             with self._lock:
@@ -517,11 +582,11 @@ class _Interface(object):
 
                 messages = []
                 while queue:
-                    messages.append(queue.pop())
+                    messages.append(queue.popleft())
 
                 if self._should_stop:
                     messages.append(StopServiceRequestMessage())
-                    logging.debug('_Interface._write_loop append(StopServiceRequestMessage)')
+                    logging.debug('_Client._write_loop append(StopServiceRequestMessage)')
 
             for msg in messages:
                 serialize(channel_out, msg)
@@ -532,40 +597,46 @@ class _Interface(object):
 
         self._channel.shutdown(socket.SHUT_WR)
 
-        logging.debug('_Interface._write_loop finished')
+        logging.debug('_Client._write_loop finished')
 
     def _read_loop(self):
         # TODO read stderr of executor to not lock
         channel_in = self._channel_in
         stop_response_received = False
 
-        logging.debug('_Interface._read_loop started')
+        logging.debug('_Client._read_loop started')
 
         while True:
             try:
                 msg = deserialize(channel_in)
             except EOFError:
-                logging.debug('_Interface._read_loop EOFError')
+                logging.debug('_Client._read_loop EOFError')
                 break
 
+            #logging.debug('_Client._read_loop %s' % msg)
+
             if isinstance(msg, ProcessStartMessage):
-                #logging.debug('_Interface._read_loop ProcessStartMessage received')
+                #logging.debug('_Client._read_loop ProcessStartMessage for task_id=%d, pid=%d, received'  % (msg.task_id, msg.pid))
+
+                pid, error = msg.pid, msg.error
 
                 with self._lock:
-                    task = self._tasks[msg.task_id]
-                pid, error = msg.pid, msg.error
+                    task = self._tasks[msg.task_id] if pid else self._tasks.pop(msg.task_id)
+
                 if error:
                     error = RuntimeError(error) # FIXME pickle Exception?
+
                 task.pid.set(pid, error)
 
             elif isinstance(msg, ProcessTerminationMessage):
-                #logging.debug('_Interface._read_loop ProcessTerminationMessage received')
+                #logging.info('_Client._read_loop ProcessTerminationMessage for task_id=%d, received'  % msg.task_id)
+
                 with self._lock:
                     task = self._tasks.pop(msg.task_id)
                     task.term_info.set(msg.exit_status)
 
             elif isinstance(msg, StopServiceResponseMessage):
-                logging.debug('_Interface._read_loop StopServiceResponseMessage received')
+                logging.debug('_Client._read_loop StopServiceResponseMessage received')
 
                 if not self._should_stop: # FIXME more fine check
                     raise RuntimeError('StopServiceResponseMessage received but StopServiceRequestMessage was not send')
@@ -581,7 +652,7 @@ class _Interface(object):
             raise RuntimeError('Socket closed without StopServiceResponseMessage')
 
         self._channel.shutdown(socket.SHUT_RD)
-        logging.debug('_Interface._read_loop finished')
+        logging.debug('_Client._read_loop finished')
 
 def dup2file(filename, newfd, flags):
     oldfd = os.open(filename, flags)
@@ -601,14 +672,14 @@ def _close_fds(white_list):
 
 def _run_executor(child_err_wr, channel):
     _close_fds([channel.fileno(), child_err_wr])
-    # XXX TODO Copy-pasted from fork_locking
-    dup2devnull(0, os.O_RDONLY)
-    dup2devnull(1, os.O_WRONLY)
 
-    #os.dup2(child_err_wr, 2) # TODO
+    dup2devnull(0, os.O_RDONLY)
+    #dup2devnull(1, os.O_WRONLY) # FIXME
+
+    #os.dup2(child_err_wr, 2) # FIXME
     os.close(child_err_wr)
 
-    ex = _Executor(channel)
+    ex = _Server(channel)
     ex.wait()
 
 def _run_executor_wrapper(*args):
@@ -633,8 +704,8 @@ def Runner():
         os.close(child_err_wr)
         channel_child.close()
         del channel_child
-        logging.debug("_Executor pid = %s" % pid)
-        return _Interface(pid, child_err_rd, channel_parent)
+        logging.debug("_Server pid = %s" % pid)
+        return _Client(pid, child_err_rd, channel_parent)
     else:
         try:
             channel_parent.close()
@@ -661,6 +732,9 @@ def CreateDefaultRunner():
 
 def Popen(*args, **kwargs):
     return DEFAULT_RUNNER.Popen(*args, **kwargs)
+
+def start(*args, **kwargs):
+    return DEFAULT_RUNNER.start(*args, **kwargs)
 
 def DestroyDefaultRunnerIfNeed():
     global DEFAULT_RUNNER
