a38a4626f9f699a5f8fde34a7948fc85ff9694d2
Trofimenkov Kirill <cgorbit@ya.ru>
2016-01-21 14:45:50 +0300

    __repr__

--- rem/future.py
+++ rem/future.py
@@ -27,6 +27,25 @@ class _FutureState(object):
         for f in to_run:
             f(future)
 
+    def _repr_inter(self):
+        is_set = self._is_set
+
+        ret = '%sset' % (
+            '' if is_set else 'not '
+        )
+
+        if is_set:
+            ret += ' to '
+            exc = self._exc
+            if exc is not None:
+                if isinstance(exc, tuple):
+                    exc = exc[1]
+                ret += type(exc).__name__ + ' exception'
+            else:
+                ret += type(self._val).__name__ + ' type'
+
+        return ret
+
     def is_set(self):
         return self._is_set
 
@@ -56,8 +75,19 @@ class _FutureState(object):
     def get(self):
         self.wait()
         if self._exc:
-            raise self._exc
+            if isinstance(self._exc, tuple):
+                raise self._exc[0], self._exc[1], self._exc[2]
+            else:
+                raise self._exc
         return self._val
+
+def _repr(self):
+    return '<%s.%s %s at 0x%x>' % (
+        self.__module__,
+        type(self).__name__,
+        self._state._repr_inter(),
+        id(self)
+    )
 
 class Future(object):
     def __init__(self, state):
@@ -78,6 +108,9 @@ class Future(object):
     def subscribe(self, code):
         self._state.subscribe(code)
 
+    def __repr__(self):
+        return _repr(self)
+
 class Promise(object):
     def __init__(self):
         self._state = _FutureState()
@@ -96,7 +129,20 @@ class Promise(object):
         exc = None
         try:
             val = code()
-        except Exception as exc:
-            pass
+        except Exception:
+            exc = sys.exc_info()
         self.set(val, exc)
 
+    def raise_and_set(self, exc):
+        def raise_():
+            raise exc
+        self.run_and_set(raise_)
+
+    def set_from_current_exception(self):
+        exc = sys.exc_info()
+        if exc[0] is None:
+            raise RuntimeError()
+        self.set(exc=exc)
+
+    def __repr__(self):
+        return _repr(self)
