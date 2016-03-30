151400360cc1d1c2fa9db09fd2d0206718f7b947
Trofimenkov Kirill <cgorbit@ya.ru>
2016-03-30 16:38:52 +0300

    Test for SerialUpdateOnlyDummyCloudClient

--- testdir/test_18.py
+++ testdir/test_18.py
@@ -45,7 +45,7 @@ recent_tags_file = %(project_dir)s/backups/recent_tags.db
 tags_db_file = %(project_dir)s/backups/tags.db
 remote_tags_db_file = %(project_dir)s/backups/tags-remote.db
 backup_dir = %(project_dir)s/backups
-backup_period = 300
+backup_period = 86400
 backup_count = 10
 backup_child_max_working_time = 900
 journal_lifetime = 3600
@@ -53,9 +53,6 @@ binary_dir = %(project_dir)s/bin
 binary_lifetime = 86400
 error_packet_lifetime = 604800
 success_packet_lifetime = 259200
-cloud_tags_server = no-such-domain-ldfkgjsghkjgfdkjgfdkj:1023
-cloud_tags_masks = file://%(project_dir)s/cloud_tags.masks
-allow_startup_tags_conversion = no
 
 [log]
 dir = %(project_dir)s/log
@@ -84,23 +81,24 @@ use_memory_profiler = no
     )
 
 
-def create_scheduler(work_dir):
+def create_default_context(work_dir):
     config_filename = work_dir + "/rem.cfg"
 
     with open(config_filename, "w") as conf:
         produce_config(conf, work_dir, hostname='foobar')
 
-    ctx = rem.context.Context(config_filename)
-
-    rem_server._init_fork_locking(ctx)
-    rem_server.init_logging(ctx)
-
-    with open(work_dir + '/cloud_tags.masks', 'w') as out:
-        print >>out, '_cloud_.*'
-
     with open(work_dir + '/network_topology.cfg', 'w') as out:
         print >>out, '[servers]'
         print >>out, 'foobar = http://localhost:8884, http://localhost:8885'
+
+    ret = rem.context.Context(config_filename)
+
+    return ret
+
+
+def create_scheduler(ctx):
+    rem_server._init_fork_locking(ctx)
+    rem_server.init_logging(ctx)
 
     return rem_server.create_scheduler(ctx)
 
@@ -136,7 +134,31 @@ def remove_journal(work_dir):
     remove_if(work_dir + '/backups', lambda file : file.startswith('recent_tags.db'))
 
 
-def testVrs(self,
+def check_uniq(iterable):
+    seen = set()
+    for v in iterable:
+        if v in seen:
+            raise ValueError("%s duplicated" % v)
+        seen.add(v)
+
+
+def get_safecloud_updates(sched):
+    ret = sched.tagRef._safe_cloud.get_state_updates()
+    check_uniq(id for id, _ in ret)
+    return [update for _, update in ret]
+
+
+def apply_updates(sched, updates):
+    for tag, ev, msg in updates:
+        sched.tagRef.AcquireTag(tag).Modify(ev, msg)
+
+
+def backup(sched):
+    sched.RollBackup()
+    time.sleep(1.5) # hack for same-timestamp-in-journal-filename problem
+
+
+def run_common_safecloud_tests(self,
             do_intermediate_backup=False,
             do_final_backup=False,
             do_remove_journal=False,
@@ -144,21 +166,14 @@ def testVrs(self,
 
     #print do_intermediate_backup, do_final_backup, do_remove_journal, do_remove_backups
 
-    def check_uniq(iterable):
-        seen = set()
-        for v in iterable:
-            if v in seen:
-                raise ValueError("%s duplicated" % v)
-            seen.add(v)
-
     def get_updates():
-        ret = sched.tagRef._safe_cloud.get_state_updates()
-        check_uniq(id for id, _ in ret)
-        return [update for _, update in ret]
+        return get_safecloud_updates(sched)
 
-    def backup():
-        sched.RollBackup()
-        time.sleep(1.5) # hack for same-timestamp-in-journal-filename problem
+    def _apply_updates(updates):
+        apply_updates(sched, updates)
+
+    def _backup():
+        backup(sched)
 
     first_part = [
         ('_cloud_tag_01', ETagEvent.Reset, 'message01'),
@@ -177,29 +192,34 @@ def testVrs(self,
 
     all_updates = first_part + second_part
 
-    def apply_updates(updates):
-        for tag, ev, msg in updates:
-            tags.AcquireTag(tag).Modify(ev, msg)
-
     with NamedTemporaryDir(prefix='remd-') as work_dir:
-        with Scheduler(work_dir) as sched:
-            tags = sched.tagRef
+        cloud_tags_masks_file = work_dir + '/cloud_tags.masks'
 
+        with open(cloud_tags_masks_file, 'w') as out:
+            print >>out, '_cloud_.*'
+
+        ctx = create_default_context(work_dir)
+
+        ctx.cloud_tags_server = 'no-such-domain-ldfkgjsghkjgfdkjgfdkj.:1023'
+        ctx.cloud_tags_masks = 'file://' + cloud_tags_masks_file
+        ctx.allow_startup_tags_conversion = False
+
+        with Scheduler(ctx) as sched:
             self.assertEqual(get_updates(), [])
 
-            apply_updates(first_part)
+            _apply_updates(first_part)
 
             self.assertEqual(get_updates(), first_part)
 
             if do_intermediate_backup:
-                backup()
+                _backup()
 
-            apply_updates(second_part)
+            _apply_updates(second_part)
 
             self.assertEqual(get_updates(), all_updates)
 
             if do_final_backup:
-                backup()
+                _backup()
 
         if do_remove_journal:
             remove_journal(work_dir)
@@ -207,7 +227,9 @@ def testVrs(self,
         if do_remove_backups:
             remove_backups(work_dir)
 
-        with Scheduler(work_dir) as sched:
+        ctx.Scheduler = None
+
+        with Scheduler(ctx) as sched:
             updates = get_updates()
 
             if do_remove_journal and do_remove_backups:
@@ -247,7 +269,164 @@ class T18(unittest.TestCase):
                                 ]
                             }
                             logging.debug(setup)
-                            testVrs(self, **setup)
+                            run_common_safecloud_tests(self, **setup)
         finally:
             rem.storages.TagStorage.CLOUD_CLIENT_STOP_TIMEOUT = original_timeout
 
+    def testCloudTagsOnDiskWithoutCloudSetup(self):
+        start_time = str(time.time())
+
+        #cloud_tags_server = 'localhost:17780'
+        cloud_tags_server = 'nanny://47821a5802574cdf9e23db6d15ec9b0d@nanny.yandex-team.ru/test_rem_cloud_tags_proxy'
+
+        def tag_name(name):
+            return name + '-' + start_time
+
+        def get_updates():
+            return get_safecloud_updates(sched)
+
+        def _apply_updates(updates):
+            apply_updates(sched, updates)
+
+        def _backup():
+            backup(sched)
+
+        #all_updates = first_part + second_part
+
+        def CloudTag(name, is_set, version):
+            return ('cloud', name, is_set, version)
+
+        def LocalTag(name, is_set):
+            return ('local', name, is_set)
+
+        def RemoteTag(name, is_set):
+            return ('remote', name, is_set)
+
+        def test_tag_from_real_tag(tag):
+            if tag.IsCloud():
+                f = lambda name, is_set: CloudTag(name, is_set, tag.version)
+            elif tag.IsRemote():
+                f = RemoteTag
+            else:
+                f = LocalTag
+
+            return f(tag.GetFullname(), tag.IsLocallySet())
+
+        def tags_dict(*tags):
+            return {t[1]: t for t in tags}
+
+        def tags_dict_from_sched():
+            return {
+                tag_name: test_tag_from_real_tag(tag)
+                    for tag_name, tag in sched.tagRef.inmem_items.iteritems()
+            }
+
+        with NamedTemporaryDir(prefix='remd-') as work_dir:
+            #cloud_tags_masks_file = work_dir + '/cloud_tags.masks'
+
+            #with open(cloud_tags_masks_file, 'w') as out:
+                #print >>out, '_cloud_.*'
+
+            ctx = create_default_context(work_dir)
+
+            ctx.cloud_tags_release_delay = 0 # XXX
+
+            ctx.cloud_tags_server = cloud_tags_server
+            ctx.all_tags_in_cloud = True
+            #ctx.cloud_tags_masks = 'file://' + cloud_tags_masks_file
+            ctx.allow_startup_tags_conversion = False
+
+            with Scheduler(ctx) as sched:
+                logging.debug('stage0')
+
+                self.assertEqual(get_updates(), [])
+
+                first_part = [
+                    (tag_name('_cloud_tag_01'), ETagEvent.Reset, 'message01'),
+                    (tag_name('_cloud_tag_02'), ETagEvent.Reset, ''),
+                    (tag_name('_cloud_tag_01'), ETagEvent.Set, None),
+                    (tag_name('_cloud_tag_01'), ETagEvent.Unset, None),
+                ]
+
+                _apply_updates(first_part)
+                time.sleep(5) # ugly
+
+                self.assertEqual(
+                    tags_dict_from_sched(),
+                    tags_dict(
+                        CloudTag(tag_name('_cloud_tag_01'), False, 3),
+                        CloudTag(tag_name('_cloud_tag_02'), False, 1),
+                    )
+                )
+
+                _backup()
+
+                self.assertEqual(tags_dict_from_sched(), {})
+
+
+            ctx.cloud_tags_server = None
+            ctx.all_tags_in_cloud = False
+            ctx.Scheduler = None
+
+            with Scheduler(ctx) as sched:
+                logging.debug('stage1')
+
+                self.assertEqual(get_updates(), [])
+                self.assertEqual(tags_dict_from_sched(), {})
+
+                second_part = [
+                    (tag_name('_cloud_tag_02'), ETagEvent.Set, None),
+                    (tag_name('_cloud_tag_02'), ETagEvent.Set, None),
+                    (tag_name('_cloud_tag_01'), ETagEvent.Reset, 'message02'),
+                    (tag_name('_cloud_tag_02'), ETagEvent.Unset, None),
+                    (tag_name('_cloud_tag_01'), ETagEvent.Set, None),
+                ]
+
+                self.assertEqual(
+                    rem.storages.SerialUpdateOnlyDummyCloudClient,
+                    type(sched.tagRef._cloud)
+                )
+
+                _apply_updates(second_part)
+                time.sleep(5) # ugly
+
+                self.assertEqual(
+                    tags_dict_from_sched(),
+                    tags_dict(
+                        CloudTag(tag_name('_cloud_tag_01'), False, 3),
+                        CloudTag(tag_name('_cloud_tag_02'), False, 1),
+                    )
+                )
+
+                _backup()
+
+                self.assertEqual(tags_dict_from_sched(), {})
+
+            def vivify_tags(tags):
+                for tag in tags:
+                    sched.tagRef.AcquireTag(tag)
+
+            ctx.cloud_tags_server = cloud_tags_server
+            ctx.all_tags_in_cloud = True
+            ctx.Scheduler = None
+
+            with Scheduler(ctx) as sched:
+                logging.debug('stage2')
+
+                self.assertEqual(get_updates(), second_part)
+                self.assertEqual(tags_dict_from_sched(), {})
+
+                vivify_tags([
+                    tag_name('_cloud_tag_01'),
+                    tag_name('_cloud_tag_02'),
+                ])
+
+                time.sleep(5) # ugly
+
+                self.assertEqual(
+                    tags_dict_from_sched(),
+                    tags_dict(
+                        CloudTag(tag_name('_cloud_tag_01'), True,  5),
+                        CloudTag(tag_name('_cloud_tag_02'), False, 4),
+                    )
+                )
