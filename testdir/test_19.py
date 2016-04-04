34c98b8695ecef9fc2292a1381b3b7df4079e7ea
Trofimenkov Kirill <cgorbit@ya.ru>
2016-04-04 13:56:01 +0300

    up

---
+++ testdir/test_19.py
@@ -0,0 +1,20 @@
+import sys
+import time
+import os
+#import logging
+
+import unittest
+
+import rem.subprocsrv as subprocsrv
+import rem.process_proxy
+#import rem.pgrpguard
+
+# TODO base_test(pgrpguard_binary=None){None, which}
+# TODO Pool
+# TODO FallbackRunner
+
+class T19(unittest.TestCase):
+    def testXXX(self):
+        runner = subprocsrv.Runner()
+
+
