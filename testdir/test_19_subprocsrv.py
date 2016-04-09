eac2a24c74a3b8c4e66fd5b514d18368d4162ca6
Trofimenkov Kirill <cgorbit@ya.ru>
2016-04-09 18:09:42 +0300

    more tests

--- testdir/test_19_subprocsrv.py
+++ testdir/test_19_subprocsrv.py
@@ -8,33 +8,34 @@ from tempfile import NamedTemporaryFile
 
 import rem.subprocsrv as subprocsrv
 import rem.subprocsrv_fallback as subprocsrv_fallback
-#import rem.job_process
-#import rem.pgrpguard
-
-# TODO base_test(pgrpguard_binary=None){None, which}
-# TODO Pool
-# TODO FallbackRunner
 
 # TODO self.setpgrp = setpgrp # useless with use_pgrpguard=True
 # TODO self.cwd = cwd # filename or None
 # TODO self.shell = shell # filename or None
-# TODO stdin_content
 
 class T19(unittest.TestCase):
-    def testXXX(self):
+    """Test rem.common.proc_runner use cases"""
+
+    def test001(self):
         self._test_common(subprocsrv.Runner(pgrpguard_binary=None), use_pgrpguard=False)
 
-    def testYYY(self):
+    def test002(self):
+        self._test_common(subprocsrv.Runner(pgrpguard_binary='pgrpguard'), use_pgrpguard=False)
+
+    def test003(self):
         self._test_common(subprocsrv.Runner(pgrpguard_binary='pgrpguard'), use_pgrpguard=True)
 
-    def testZZZ(self):
+    def test004(self):
+        self._test_common(subprocsrv_fallback.Runner())
+
+    def test005(self):
         runner = subprocsrv_fallback.RunnerWithFallback(
             main=subprocsrv.BrokenRunner(),
             fallback=subprocsrv_fallback.Runner()
         )
         self._test_common(runner)
 
-    def _test_common(self, runner, use_pgrpguard=False):
+    def _do_test_common(self, runner, use_pgrpguard=False, use_stdin_content=False):
         msg = str(time.time())
 
         with NamedTemporaryFile('w') as stdin:
@@ -44,10 +45,12 @@ class T19(unittest.TestCase):
                     stdin.flush()
 
                     p = runner.Popen(
-                        'cat; echo -n Error >&2; exit 3',
+                        'cat; pwd >&2; exit 3',
+                        cwd='/proc',
                         shell=True,
                         setpgrp=False,
-                        stdin=stdin.name,
+                        stdin=stdin.name if not use_stdin_content else None,
+                        stdin_content=msg if use_stdin_content else None,
                         stdout=stdout.name,
                         stderr=stderr.name,
                         use_pgrpguard=use_pgrpguard
@@ -56,5 +59,10 @@ class T19(unittest.TestCase):
                     self.assertEqual(p.wait(), 3)
 
                     self.assertEqual(stdout.read(), msg)
-                    self.assertEqual(stderr.read(), 'Error')
+                    self.assertEqual(stderr.read(), '/proc\n')
 
+    def _test_common(self, runner, use_pgrpguard=False):
+        for use_stdin_content in [False, True]:
+            self._do_test_common(runner,
+                                 use_pgrpguard=use_pgrpguard,
+                                 use_stdin_content=use_stdin_content)
