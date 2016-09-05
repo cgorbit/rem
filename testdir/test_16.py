dcc6b645c5ce74243e62ada715048f516bd834c6
Trofimenkov Kirill <trofimenkov@yandex-team.ru>
2016-09-05 12:38:23 +0300

    test SuspendedLifetime

--- testdir/test_16.py
+++ testdir/test_16.py
@@ -14,11 +14,47 @@ class T16(unittest.TestCase):
 
     def testCustomQueueSuccessLifetime(self):
         timestamp = time.time()
-        pck = self.connector.Packet('test_lifetime-%d' % int(timestamp))
+        pck = self.connector.Packet('test_successfull_lifetime-%d' % int(timestamp))
         pck.AddJob('true')
         self.connector.Queue('test_lifetime').AddPacket(pck)
         pckInfo = self.connector.PacketInfo(pck.id)
-        self.connector.Queue('test_lifetime').SetSuccessLifeTime(1)
         WaitForExecution(pckInfo, ["SUCCESSFULL"])
-        RestartService(Config.Get().server1.projectDir)
+
+        self.connector.Queue('test_lifetime').SetSuccessLifeTime(1)
+        time.sleep(1)
+        self.connector.proxy.forget_old_items()
+
         self.assertRaises(pckInfo.update)
+
+    def testCustomQueueSuccessLifetime(self):
+        timestamp = time.time()
+        pck = self.connector.Packet('test_error_lifetime-%d' % int(timestamp))
+        pck.AddJob('false', tries=1)
+        self.connector.Queue('test_lifetime').AddPacket(pck)
+        pckInfo = self.connector.PacketInfo(pck.id)
+        WaitForExecution(pckInfo, ["ERROR"])
+
+        self.connector.Queue('test_lifetime').SetErroredLifeTime(1)
+        time.sleep(1)
+        self.connector.proxy.forget_old_items()
+
+        self.assertRaises(pckInfo.update)
+
+    def testCustomQueueSuccessLifetime(self):
+        timestamp = time.time()
+        queue = self.connector.Queue('test_lifetime_%s' % timestamp)
+
+        pck = self.connector.Packet('test_suspended_lifetime-%d' % int(timestamp),
+            wait_tags=['no_such_tag_%s' % timestamp])
+        pck.AddJob('true')
+        queue.AddPacket(pck)
+
+        pckInfo = self.connector.PacketInfo(pck.id)
+
+        WaitForExecution(pckInfo, ["SUSPENDED"])
+
+        queue.SetSuspendedLifeTime(1)
+        time.sleep(1)
+        self.connector.proxy.forget_old_items()
+
+        WaitForExecution(pckInfo, ["ERROR"])
